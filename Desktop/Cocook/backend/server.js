aabeab21e1e93251e301712330d8d2f302c9db4c
konanki-vasundhara <konankivasundhara@gmail.com>
2026-05-26 15:59:36 +0530

    Update server.js

--- Desktop/Cocook/backend/server.js
+++ Desktop/Cocook/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // --- Mock Database ---
