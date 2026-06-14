de7972ea4229bfaf0006bd9f053cb48ce10eb2d3
konanki-vasundhara <konankivasundhara@gmail.com>
2026-06-14 18:23:53 +0530

    Update main.py

--- Desktop/Cocook/backend/main.py
+++ Desktop/Cocook/backend/main.py
@@ -34,12 +34,10 @@ allowed_origins = (
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [
-    "http://localhost:5173",
-    "http://172.31.44.161:5173",
-    "http://13.63.237.243:5173",
-    "http://13.63.237.243",
-],
+    allow_origins=[
+        "http://13.63.237.243:3000",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
